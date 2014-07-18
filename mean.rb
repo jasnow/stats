59b8950501a826f9a59e4b76bf3a150216a8d5fe
Al Snow <jasnow@hotmail.com>
2014-07-18 18:06:59 -0400

    Possible answer

--- mean.rb
+++ mean.rb
@@ -3,35 +3,27 @@
 
 data = [2, 3, 1, 6, 53, 10, 7, 33, 22, 22]
 
-def sumx values
-  sum = 0
-  values.each do |x|
-    sum += x
-  end
-  puts('The sum equals ' + sum.to_s)
-  #puts('The mean equals ' + (sum/values.length).to_s)
-end
+def sum values
 
-puts
-puts(sumx data)
-puts
+  # ANSWER: http://stackoverflow.com/questions/1538789/how-to-sum-array-of-numbers-in-ruby
+  s = values.inject(:+)
+
+  puts 'The sum equals ' + s.to_s
+  puts 'The mean equals ' + (s/values.length).to_s
+  s
+end
 
 def sum_squares values1
   sum_squares = 0
-  #xbar = 0
-  xbar = 0 + sumx(values1).to_i
-  puts "x=" + xbar.to_s
+  xbar = sum(values1)
+  puts "The sum equals " + xbar.to_s
+
   values1.each do |x|
     sum_squares += (x - xbar)**2
   end
+
   puts('The squared sums equal ' + sum_squares.to_s)
   puts('The stdev equals ' + (Math.sqrt(sum_squares)/(values1.length-1)).to_s)
 end
 
-puts
 sum_squares data
-puts
-
-
-
-
